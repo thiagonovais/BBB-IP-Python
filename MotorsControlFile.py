183e75d3fc419122d8a7bd79648c8367679a2fc6
thiagonovais <thiago.novais@gmail.com>
2017-06-01 15:15:38 -0300

    Update MotorsControlFile.py

--- MotorsControlFile.py
+++ MotorsControlFile.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python
  
 # Python Standart Library Imports
-import SabertoothFile
+
 import PWMFile
-import ProBotConstantsFile
+import ConstantsFile
 
 # Initialization of classes from local files
-Sabertooth = SabertoothFile.SabertoothClass()
+
 PWM = PWMFile.PWMClass()
-Pconst = ProBotConstantsFile.Constants()
+Pconst = ConstantsFile.Constants()
 
 
 class MotorsControlClass():
     
-    def MotorsControl(self,rightMotor, leftMotor, userChoice):
-	if userChoice=='1':
-		# Sending the values to the Sabertooth that is connected to the motors
-		Sabertooth.drive(Pconst.addr, 1, int(rightMotor))
-	        Sabertooth.drive(Pconst.addr, 2, int(leftMotor))
-
-	if userChoice=='2':
+    def MotorsControl(self,rightMotor, leftMotor):
 		# Sending the values to the pwm controller that is connected to the motors
 		PWM.PWM_Signals(round(rightMotor, 2), round(leftMotor, 2))
